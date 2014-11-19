9aad7f57ac971ad28c83d4d4cad0cc006af27b1c
Anubhav Saxena <anubhavumich@gmail.com>
2014-11-18 20:23:20 -0500

    Layout changes for Login screen, files for landscape and portrait added

--- Fete/app/src/main/java/com/group9/fete/adapter/FeaturedVenueAdapter.java
+++ Fete/app/src/main/java/com/group9/fete/adapter/FeaturedVenueAdapter.java
@@ -6,10 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 import com.group9.fete.R;
 import com.group9.fete.model.Venue;
-
 import java.util.List;
 
 /**
@@ -30,14 +29,16 @@ public class FeaturedVenueAdapter extends ArrayAdapter<Venue> {
         LayoutInflater inflator = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         //Get featured_venue_layout from inflator
-        View featuredVenues = inflator.inflate(R.layout.featured_venue_layout, parent, false);
+        View featuredVenues = inflator.inflate(R.layout.search_result_layout, parent, false);
 
         //Fill images in imageView
         ImageView imageView = (ImageView)featuredVenues.findViewById(R.id.venueImage);
-        venueList.get(position).GetVenueImage();
-        int resId = context.getResources().getIdentifier(venueList.get(position).GetVenueImage(), "drawable", context.getPackageName());
+        String imagePath = venueList.get(position).GetVenueImage();
+        int resId = context.getResources().getIdentifier(imagePath, "drawable", context.getPackageName());
         imageView.setImageResource(resId);
-
+        String venueName = venueList.get(position).GetVenueName();
+        TextView venueNameTextView = (TextView)featuredVenues.findViewById(R.id.venueName);
+        venueNameTextView.setText(venueName);
         return featuredVenues;
     }
 }
