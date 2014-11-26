a0161bde1d4f2fcdb7de01a2d04a50b9866fa2c9
Allysin <atmackay@umich.edu>
2014-11-25 20:11:29 -0500

    Trying to fix this mess

--- Fete/app/src/main/java/com/group9/fete/adapter/FeaturedVenueAdapter.java
+++ Fete/app/src/main/java/com/group9/fete/adapter/FeaturedVenueAdapter.java
@@ -1,38 +1,11 @@
-<<<<<<< HEAD
-package com.group9.fete.adapter;
-
-import android.content.Context;
-import android.widget.ArrayAdapter;
-
-import com.group9.fete.model.Venue;
-
-import java.util.List;
-
-/**
- * Created by Anubhav on 05-11-2014.
- */
-public class FeaturedVenueAdapter extends ArrayAdapter<Venue> {
-    private final Context context;
-    private final List<Venue> venueList;
-
-    public FeaturedVenueAdapter(Context c, int resourceId , List<Venue> items){
-        super(c, resourceId, items);
-        this.context = c;
-        this.venueList = items;
-    }
-
-=======
+//
 //package com.group9.fete.adapter;
 //
 //import android.content.Context;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
 //import android.widget.ArrayAdapter;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import com.group9.fete.R;
+//
 //import com.group9.fete.model.Venue;
+//
 //import java.util.List;
 //
 ///**
@@ -48,26 +21,53 @@ public class FeaturedVenueAdapter extends ArrayAdapter<Venue> {
 //        this.venueList = items;
 //    }
 //
->>>>>>> parent of 63e9174... Revert 555ceea..55af558
-//    public View getView(int position, View convertView, ViewGroup parent){
-//        //Create inflator
-//        LayoutInflater inflator = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//
-//        //Get featured_venue_layout from inflator
-//        View featuredVenues = inflator.inflate(R.layout.search_result_layout, parent, false);
-//
-//        //Fill images in imageView
-//        ImageView imageView = (ImageView)featuredVenues.findViewById(R.id.venueImage);
-//        String imagePath = venueList.get(position).GetVenueImage();
-//        int resId = context.getResources().getIdentifier(imagePath, "drawable", context.getPackageName());
-//        imageView.setImageResource(resId);
-//        String venueName = venueList.get(position).GetVenueName();
-//        TextView venueNameTextView = (TextView)featuredVenues.findViewById(R.id.venueName);
-//        venueNameTextView.setText(venueName);
-//        return featuredVenues;
-//    }
-<<<<<<< HEAD
-}
-=======
+//=======
+////package com.group9.fete.adapter;
+////
+////import android.content.Context;
+////import android.view.LayoutInflater;
+////import android.view.View;
+////import android.view.ViewGroup;
+////import android.widget.ArrayAdapter;
+////import android.widget.ImageView;
+////import android.widget.TextView;
+////import com.group9.fete.R;
+////import com.group9.fete.model.Venue;
+////import java.util.List;
+////
+/////**
+//// * Created by Anubhav on 05-11-2014.
+//// */
+////public class FeaturedVenueAdapter extends ArrayAdapter<Venue> {
+////    private final Context context;
+////    private final List<Venue> venueList;
+////
+////    public FeaturedVenueAdapter(Context c, int resourceId , List<Venue> items){
+////        super(c, resourceId, items);
+////        this.context = c;
+////        this.venueList = items;
+////    }
+////
+//>>>>>>> parent of 63e9174... Revert 555ceea..55af558
+////    public View getView(int position, View convertView, ViewGroup parent){
+////        //Create inflator
+////        LayoutInflater inflator = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+////
+////        //Get featured_venue_layout from inflator
+////        View featuredVenues = inflator.inflate(R.layout.search_result_layout, parent, false);
+////
+////        //Fill images in imageView
+////        ImageView imageView = (ImageView)featuredVenues.findViewById(R.id.venueImage);
+////        String imagePath = venueList.get(position).GetVenueImage();
+////        int resId = context.getResources().getIdentifier(imagePath, "drawable", context.getPackageName());
+////        imageView.setImageResource(resId);
+////        String venueName = venueList.get(position).GetVenueName();
+////        TextView venueNameTextView = (TextView)featuredVenues.findViewById(R.id.venueName);
+////        venueNameTextView.setText(venueName);
+////        return featuredVenues;
+////    }
+//<<<<<<< HEAD
 //}
->>>>>>> parent of 63e9174... Revert 555ceea..55af558
+//=======
+////}
+//>>>>>>> parent of 63e9174... Revert 555ceea..55af558
